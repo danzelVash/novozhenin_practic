1b285306a06f90ef7eee46d295151293bb712a7a
duzyanov <duzyanov@ozon.ru>
2026-03-04 21:15:37 +0300

    impl master slave

--- internal/master/neuro/gateway.go
+++ internal/master/neuro/gateway.go
@@ -29,7 +29,7 @@ func (g *Gateway) Recognize(ctx context.Context, audio []byte) (string, error) {
 		Chunk: audio,
 		Commands: []*pb.GetAudioRequest_Command{
 			{Name: "вверх"},
-			{Name: "ниже"},
+			{Name: "вниз"},
 			{Name: ""},
 		},
 	})
